ca8f99a75ced20d10e82f0468cee1222a20018aa
Issy Codron <ic302@exeter.ac.uk>
2026-05-16 11:07:48 +0100

    add display.py with status and urgency display functions

---
+++ src/display.py
@@ -0,0 +1,44 @@
+'''
+display.py is responsible for displaying the status of Meeko's feeding schedule.
+This includes a trigger feature to show days since/days until next feeding, 
+and an urgency score that can be used to control the LED display on the physical device.
+'''
+
+
+def display_status(status):
+    """
+    This function will be responsible for displaying the status of Meeko's feeding schedule.
+    It will show days since last feeding, days until next feeding.
+    For now, this will just print to the console, but eventually it will be triggered by a button on the console.
+    """
+    # make border display for console output
+
+    print("="*40)
+    print(status)
+    print("="*40)
+
+
+def display_urgency(urgency_score, days_since):
+    """
+    This function will take the urgency score calculated in feeder.py and use it to control the LED display on the physical device.
+    For example, it could change the color or brightness of the LED based on the urgency score.
+    For now, this will just print the urgency score to the console, but eventually it will control the LED display on the physical device.
+    """
+    print(f"Urgency Score: {urgency_score}")
+    if urgency_score >= 1.4:
+        print("LED Color: Flashing colours (Very Urgent)")
+        print(f"Urgent Warning: Meeko is very hungry! Days since last feeding: {days_since}")
+    elif urgency_score >= 1.2:
+        print("LED Color: Flashing red (Urgent)")
+        print(f"Warning: Meeko is hungry! Days since last feeding: {days_since}")
+    elif urgency_score >= 1.0:
+        print("LED Color: Red (Feed the beast!)")
+    elif urgency_score >= 0.8:
+        print("LED Color: Orange (Get Ready)")
+    elif urgency_score >= 0.6:
+        print("LED Color: Yellow (Has he pooped?)")
+    else:
+        print("LED Color: Green (Not Urgent)")
+
+
+
--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ Script that runs the functions in feeder.py
 # Import necessary modules
 from datetime import date
 from feeder import log_feeding, read_last_feeding_date, next_feeding_date, days_since_feeding, calculate_urgency_score, get_status_updates
+from display import display_status, display_urgency
 
 # run feeder.py here. initial testing
 
@@ -12,13 +13,10 @@ if __name__ == "__main__":
     last_feeding = read_last_feeding_date()
     if last_feeding:
         status = get_status_updates()
-        print(status)
+        display_status(status)
         days_since = days_since_feeding(last_feeding)
         urgency_score = calculate_urgency_score(days_since)
-        if urgency_score >= 1.4:
-            print(f"Urgent Warning: Meeko is very hungry! Days since last feeding: {days_since}")
-        elif urgency_score >= 1.2:
-            print(f"Warning: Meeko is hungry! Days since last feeding: {days_since}")
+        display_urgency(urgency_score, days_since)
     else:
         print("No feeding data found.")
 
